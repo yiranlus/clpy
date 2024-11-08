7b232faa9617ce4f3fb1bbd233e34fb641696b91
Yiran Lu <yiranls@outlook.com>
2024-11-08 21:32:26 +0100

    change library version

--- src/library.lisp
+++ src/library.lisp
@@ -4,8 +4,8 @@
 (in-package :clpy.library)
 
 (cffi:define-foreign-library libpython
-  (:unix (:or "libpython3.11.so.0" "libpython3.11.so.1" "libpython3.11.so"))
-  (:windows "python3.11.dll")
-  (t (:default "libpython3.11")))
+  (:unix (:or "libpython3.so.0" "libpython3.so.1" "libpython3.so"))
+  (:windows "python3.dll")
+  (t (:default "libpython3")))
 
 (cffi:use-foreign-library libpython)
